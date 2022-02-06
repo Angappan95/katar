0b1fe97591ed6b993b19b60b7dd566b718467354
Angappan <angappan.ganesan@walmart.com>
2022-02-06 23:14:34 +0530

    First commit.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //@ts-check
 import fs from 'fs';
+const authorBlocksRe = /\*\*(.|)+[\s\S]*?(?=\n.*=|\/)/i
 const authorRe = /(?<=\@author)[\w:, ]+/i
 const descriptionRe = /(?<=\@function_description)[\w\s: ]+(?=\*)/i
 const paramsRe = /(?<=input Params)[\w:, ]+/i
@@ -8,20 +9,24 @@ const methodRe = /(?<=async).+(?=\{)/i
 
 var x;
 
-fs.readFile('resources/Script1.js', 'utf8', function(err, data) {
+fs.readFile('resources/Script1.js', 'utf8', function (err, data) {
     if (!err) {
-        x = data;
-        // console.log(x)
-        let methodName = x.match(methodRe)
-        let description = x.match(descriptionRe)
-        let params = x.match(paramsRe)
-        let author = x.match(authorRe)
-        let return_ = x.match(returnRe)
-        console.log(`Method Signature: ${methodName[0].trim().replace("function", "")}`)
-        console.log(`Description: ${description[0].trim().replace(":", "")}`)
-        console.log(`Input Params: ${params[0].trim().replace(":", "")}`)
-        console.log(`Return: ${return_[0].trim().replace(":", "")}`)
-        console.log(`Author: ${author[0].trim().replace(":", "")}`)
+        // console.log(data)
+        let authorBlocks = data.match(authorBlocksRe)
+        if (authorBlocks.length > 0) {
+            authorBlocks.forEach(x => {
+                let methodName = x.match(methodRe)
+                let description = x.match(descriptionRe)
+                let params = x.match(paramsRe)
+                let author = x.match(authorRe)
+                let return_ = x.match(returnRe)
+                console.log(`Method Signature: ${methodName[0].trim().replace("function", "")}`)
+                console.log(`Description: ${description[0].trim().replace(":", "")}`)
+                console.log(`Input Params: ${params[0].trim().replace(":", "")}`)
+                console.log(`Return: ${return_[0].trim().replace(":", "")}`)
+                console.log(`Author: ${author[0].trim().replace(":", "")}`)
+            })
+        }
     } else {
         console.log(`Something went wrong ${err}`)
     }
--- resources/Script1.js
+++ resources/Script1.js
@@ -9,3 +9,13 @@ var path = require("path");
 export async function validateAddition(num1, num2) {
     return num1 + num2
 }
+
+/**
+     * @author : Angappan
+     * @function_description : Validate the addition functionality 
+     * @input Params : Number, Number
+     * @return : Sum of number1 & number2
+    **/
+export async function validateSubraction(num1, num2) {
+    return num1 - num2
+}
