c91f42ecaa7abf2e205ef3550c65df801076e151
Angappan <angappan.ganesan@walmart.com>
2022-02-07 14:22:38 +0530

    Updated regex

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //@ts-check
 import fs from 'fs';
-const authorBlocksRe = /\*\*(.|)+[\s\S]*?(?=\n.*=|\/)/i
+const authorBlocksRe = /\*\*(.|)+[\s\S]*?(?=\n.*=|\{)/gi
 const authorRe = /(?<=\@author)[\w:, ]+/i
 const descriptionRe = /(?<=\@function_description)[\w\s: ]+(?=\*)/i
 const paramsRe = /(?<=input Params)[\w:, ]+/i
 const returnRe = /(?<=\@return)[\w:, ]+/i
-const methodRe = /(?<=async).+(?=\{)/i
+const methodRe = /(?<=async).+(?=\))/i
 
 var x;
 
@@ -13,6 +13,7 @@ fs.readFile('resources/Script1.js', 'utf8', function (err, data) {
     if (!err) {
         // console.log(data)
         let authorBlocks = data.match(authorBlocksRe)
+        // console.log(authorBlocks)
         if (authorBlocks.length > 0) {
             authorBlocks.forEach(x => {
                 let methodName = x.match(methodRe)
@@ -20,11 +21,12 @@ fs.readFile('resources/Script1.js', 'utf8', function (err, data) {
                 let params = x.match(paramsRe)
                 let author = x.match(authorRe)
                 let return_ = x.match(returnRe)
-                console.log(`Method Signature: ${methodName[0].trim().replace("function", "")}`)
+                console.log(`Method Signature: ${methodName[0].trim().replace("function", "")})`)
                 console.log(`Description: ${description[0].trim().replace(":", "")}`)
                 console.log(`Input Params: ${params[0].trim().replace(":", "")}`)
                 console.log(`Return: ${return_[0].trim().replace(":", "")}`)
                 console.log(`Author: ${author[0].trim().replace(":", "")}`)
+                console.log(`********************************`)
             })
         }
     } else {
