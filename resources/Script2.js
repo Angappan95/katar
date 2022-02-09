90adf740b0e3ee131391d4a964200da14a648356
Angappan <angappan.ganesan@walmart.com>
2022-02-09 18:34:47 +0530

    Added Script2 for testing

---
+++ resources/Script2.js
@@ -0,0 +1,21 @@
+var path = require("path");
+
+/**
+     * @author : Angappan
+     * @function_description : Validate the Multiplication functionality 
+     * @input Params : Number, Number
+     * @return : Product of number1 & number2
+    **/
+export async function validateMultiplication(num1, num2) {
+    return num1 * num2
+}
+
+/**
+     * @author : Angappan
+     * @function_description : Validate the Division functionality 
+     * @input Params : Number, Number
+     * @return : Quotient of number1 & number2
+    **/
+export async function validateDivision(num1, num2) {
+    return num1 / num2
+}
