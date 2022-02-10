11b4deeac7cfe91af9b5883f0dcc1d8ec3ffdedd
Angappan <angappan.ganesan@walmart.com>
2022-02-10 08:32:19 +0530

    1. Added getAllFiles func to recursively get files 2. Modified logic to get the author block from all the recursive files

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
 //@ts-check
 import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 const authorBlocksRe = /\*\*(.|)+[\s\S]*?(?=\n.*=|\{)/gi
 const authorRe = /(?<=\@author)[\w:, ]+/i
 const descriptionRe = /(?<=\@function_description)[\w\s: ]+(?=\*)/i
 const paramsRe = /(?<=input Params)[\w:, ]+/i
 const returnRe = /(?<=\@return)[\w:, ]+/i
 const methodRe = /(?<=async).+(?=\))/i
-const dirname = "resources";
+const dirpath = "resources";
 
-fs.readdir(dirname, (err, filenames) => {
-    if (err) {
-        console.log(`Something went wrong ${err}`);
-        return;
-    }
 
-    console.log(`Filename => ${filenames}`)
-    
-    filenames.forEach(filename => {
-        fs.readFile(`${dirname}/${filename}`, 'utf8', function (err, data) {
-            if (!err) {
-                // console.log(data)
-                let authorBlocks = data.match(authorBlocksRe)
-                // console.log(authorBlocks)
-                if (authorBlocks.length > 0) {
-                    authorBlocks.forEach(x => {
-                        let methodName = x.match(methodRe)
-                        let description = x.match(descriptionRe)
-                        let params = x.match(paramsRe)
-                        let author = x.match(authorRe)
-                        let return_ = x.match(returnRe)
-                        console.log(`Method Signature: ${methodName[0].trim().replace("function", "")})`)
-                        console.log(`Description: ${description[0].trim().replace(":", "")}`)
-                        console.log(`Input Params: ${params[0].trim().replace(":", "")}`)
-                        console.log(`Return: ${return_[0].trim().replace(":", "")}`)
-                        console.log(`Author: ${author[0].trim().replace(":", "")}`)
-                        console.log(`********************************`)
-                    })
-                }
-            } else {
-                console.log(`Something went wrong ${err}`)
+const getAllFiles = (dirPath, arrayOfFiles) => {
+    let files = fs.readdirSync(dirPath)
+
+    arrayOfFiles = arrayOfFiles || []
+
+    files.forEach((file) => {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+            arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+        } else {
+            arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+        }
+    })
+
+    return arrayOfFiles
+}
+
+var filenames = getAllFiles(dirpath)
+filenames.forEach(filename => {
+    fs.readFile(filename, 'utf8', function (err, data) {
+        if (!err) {
+            // console.log(data)
+            let authorBlocks = data.match(authorBlocksRe)
+            // console.log(authorBlocks)
+            if (authorBlocks.length > 0) {
+                authorBlocks.forEach(x => {
+                    let methodName = x.match(methodRe)
+                    let description = x.match(descriptionRe)
+                    let params = x.match(paramsRe)
+                    let author = x.match(authorRe)
+                    let return_ = x.match(returnRe)
+                    console.log(`Method Signature: ${methodName?.[0].trim().replace("function", "")})`)
+                    console.log(`Description: ${description?.[0].trim().replace(":", "")}`)
+                    console.log(`Input Params: ${params?.[0].trim().replace(":", "")}`)
+                    console.log(`Return: ${return_?.[0].trim().replace(":", "")}`)
+                    console.log(`Author: ${author?.[0].trim().replace(":", "")}`)
+                    console.log(`********************************`)
+                })
             }
-        })
+        } else {
+            console.log(`Something went wrong ${err}`)
+        }
     })
 })
+
 
 
