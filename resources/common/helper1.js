50e4015d22db70f379817269fef0e1f47c357816
Angappan <angappan.ganesan@walmart.com>
2022-02-10 08:23:28 +0530

    Added sub dir for testing

--- resources/common/helper1.js
+++ resources/common/helper1.js
@@ -2,9 +2,20 @@
      * @author : Angappan
      * @function_description : Validate the given variable is Integer
      * @input Params : Number
-     * @return : Whether given number is Integer - {boolean}
+     * @return : Whether given number is Integer - <boolean>
     **/
 
 export async function isNumber(num) {
     return Number.isNumber(num)
+}
+
+/**
+     * @author : Angappan
+     * @function_description : Validate the given variable is Null
+     * @input Params : Number
+     * @return : Whether given number is Null - <boolean>
+    **/
+
+ export async function isNull(num) {
+    return Number.isNull(num)
 }
---
+++ resources/common/utils/utils.js
@@ -0,0 +1,9 @@
+/**
+     * @author : Angappan
+     * @function_description : Validate the given variable is Integer
+     * @input Params : Number
+     * @return : Whether given number is Integer - {boolean}
+    **/
+ export async function isNumber(num) {
+    return Number.isNumber(num)
+}
